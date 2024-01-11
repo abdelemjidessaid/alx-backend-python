d83ff108eaf7a9c8e57e89cdee7bade066b14c9d
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-01-11 12:15:27 +0100

    1st task is done

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+    Module of add function
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+        Function that adds two floating numbers
+
+        return:
+            It returns the addition of the two floating numbers
+    """
+
+    return a + b
