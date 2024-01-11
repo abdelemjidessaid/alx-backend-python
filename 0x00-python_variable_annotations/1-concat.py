3929e5113b51faaf467e2d6fdf0e67c5082ef95f
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-01-11 12:19:35 +0100

    2nd task is done

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+    Module that contains the concat function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        Function that concatenates two strings
+
+        return:
+            This function returns the result of concatenation
+    """
+
+    return str1 + str2
