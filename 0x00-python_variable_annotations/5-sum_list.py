bdecc2d6875fbdbd11545cd256ed7bf7cec72454
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-01-11 14:21:24 +0100

    6th task is done

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+    Module of sum_list function
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+        Function that returns the sum of a list elements
+    """
+
+    sum: float = 0
+    for n in input_list:
+        sum = sum + n
+    return sum
