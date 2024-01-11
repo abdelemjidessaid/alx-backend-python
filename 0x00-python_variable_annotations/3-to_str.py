f04f8e7a88cccfdab88c4338c6a61c6c90094d52
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-01-11 12:29:41 +0100

    4th task is done

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+    Module of to_str function
+"""
+
+
+def to_str(n: float) -> str:
+    """
+        Function that cast a float number to string
+
+        Return:
+            returns the a string hold the same float number
+    """
+
+    return str(n)
