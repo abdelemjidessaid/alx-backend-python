09d4bfba6ae1815b535bdd17d47e3534be00472f
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-02-02 12:49:33 +0100

    7th task is done

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -40,3 +40,26 @@ class TestGithubOrgClient(unittest.TestCase):
         with patch(mocked_method, PropertyMock(return_value=payload)):
             client = GithubOrgClient('heroshima')
             self.assertEqual(client._public_repos_url, expected)
+
+    @patch('client.get_json')
+    def test_public_repos(self, get_json_mock):
+        """
+            Method that tests the GithubOrgClient.public_repos Method
+        """
+        abdo = {"name": "Abdo", "license": {"key": "a"}}
+        ess = {"name": "Ess", "license": {"key": "b"}}
+        user = {"name": "User"}
+        to_mock = 'client.GithubOrgClient._public_repos_url'
+        get_json_mock.return_value = [abdo, ess, user]
+        with patch(
+            to_mock, PropertyMock(return_value="abdelemjidessaid")
+        ) as y:
+            heroshima = GithubOrgClient("heroshima")
+            self.assertEqual(
+                heroshima.public_repos(), ['Abdo', 'Ess', 'User']
+            )
+            self.assertEqual(heroshima.public_repos("a"), ['Abdo'])
+            self.assertEqual(heroshima.public_repos("c"), [])
+            self.assertEqual(heroshima.public_repos(45), [])
+            get_json_mock.assert_called_once_with("abdelemjidessaid")
+            y.assert_called_once_with()
